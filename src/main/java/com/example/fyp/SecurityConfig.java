25228f05df9abf0cf1b7b796d0ed8eb954b346d8
jacky <applelala44@gmail.com>
2024-06-17 02:00:52 +0800

    update security config

--- src/main/java/com/example/fyp/SecurityConfig.java
+++ src/main/java/com/example/fyp/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.fyp;
 
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
+    // jwt secret is for HMAC symmetric authentication
+    // @Value("${jwt.secret}")
+    // private String jwtSecret;
+
+    @Value("${cognito.region}")
+    private String cognitoRegion;
+
+    @Value("${cognito.userPoolId}")
+    private String userPoolId;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -36,7 +41,13 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256")).build();
+        String jwkSetUri = "https://cognito-idp." + cognitoRegion + ".amazonaws.com/" + userPoolId + "/.well-known/jwks.json";
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
+
+    // @Bean
+    // public JwtDecoder jwtDecoder() {
+    //     return NimbusJwtDecoder.withSecretKey(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256")).build();
+    // }
 
 }
