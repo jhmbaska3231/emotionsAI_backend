adc9a3b2d92b4b4aa59b175e7cb9d534aef40a8d
jacky <applelala44@gmail.com>
2024-06-17 04:25:50 +0800

    added code for lambda public key

--- src/main/java/com/example/fyp/SecurityConfig.java
+++ src/main/java/com/example/fyp/SecurityConfig.java
@@ -1,5 +1,13 @@
 package com.example.fyp;
 
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // jwt secret is for HMAC symmetric authentication
+    // working code to create user from lambda trigger using symmetric key
     // @Value("${jwt.secret}")
     // private String jwtSecret;
 
@@ -22,6 +30,9 @@ public class SecurityConfig {
 
     @Value("${cognito.userPoolId}")
     private String userPoolId;
+
+    @Value("${lambda.public.key}")
+    private String publicKeyContent;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,6 +56,30 @@ public class SecurityConfig {
         return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 
+    @Bean
+    public JwtDecoder lambdaJwtDecoder() throws IOException, InvalidKeySpecException {
+        RSAPublicKey publicKey = buildRSAPublicKey(publicKeyContent);
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    private RSAPublicKey buildRSAPublicKey(String publicKeyContent) throws InvalidKeySpecException {
+        // Remove the "BEGIN" and "END" markers
+        publicKeyContent = publicKeyContent.replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                                           .replaceAll("-----END PUBLIC KEY-----", "")
+                                           .replaceAll("\\s+", ""); // Remove any whitespace characters
+    
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyContent);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+    
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new InvalidKeySpecException("Error generating RSA public key", e);
+        }
+    }
+    
+    // working code to create user from lambda trigger using symmetric key
     // @Bean
     // public JwtDecoder jwtDecoder() {
     //     return NimbusJwtDecoder.withSecretKey(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256")).build();
