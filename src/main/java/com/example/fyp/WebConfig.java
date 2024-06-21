1509d98a6c7d4ab4b8312659e8f99ba4c78a0e2e
jacky <applelala44@gmail.com>
2024-06-21 23:58:58 +0800

    updated web config

--- src/main/java/com/example/fyp/WebConfig.java
+++ src/main/java/com/example/fyp/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://staging.ds3vg2zxhq1q5.amplifyapp.com/")
+                        .allowedOrigins("http://localhost:3000", "https://staging.ds3vg2zxhq1q5.amplifyapp.com/", "https://emotionsai.space/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
